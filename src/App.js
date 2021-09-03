010f003ad50b316fc90047c74e7571e40da8d195
badal <badal1906@gmail.com>
2021-09-04 00:57:20 +0530

    added search feature and completed the ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import"./App.css"
-import Header from './Header'
+import "./App.css";
+import Header from "./Header";
 import RecommendedVideos from "./RecommendedVideos";
 import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage"
+
 
 function App() {
   return (
-    <div >
-      <Header/>
-      <div className="app__page">
-        <Sidebar/>
-        <RecommendedVideos/>
-      </div>
-      
+    <div>
+      <Router>
+      <Header />
+        <Switch>
+          <Route path="/search/:searchTerm">{/*it will render the page with the keyword we have searched in the search bar */}
+          <div className="app__page">
+              <Sidebar />
+              <SearchPage />
+            </div>
+          
+          </Route>
+          <Route path="/">
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState } from "react";
 import "./Header.css";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
@@ -7,22 +7,31 @@ import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom"
 
 function Header() {
+  const [inputSearch,setInputSeacrh] = useState('');
+
   return (
     <div className="header">
       <div className="header__left">
         <MenuIcon />
+        <Link to ={'/'}>
         <img
           className="header__logo"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png"
         />
+        </Link >
       </div>
 
       <div className="header__input">
-        <input placeholder="Search" type="text" />
+        <input onChange={e => setInputSeacrh(e.target.value)} value={inputSearch} placeholder="Search" type="text" />
+        
+        <Link to ={`/search/${inputSearch}`}>{/*shows searched keyword in url */}
         <SearchIcon className="header__inputButton"/>
+        </Link>
       </div>
+
       <div className="header__icons">
       <VideoCallIcon className="header__icon"/>
       <AppsIcon className="header__icon" />
